a5cae76bc0ca7b9064f4463b2ab5f0e6a4532fee
DenysTkachov <D.Tkachev@ukr.net>
2023-10-28 09:59:19 +0200

    Profile

---
+++ src/components/Profile.js
@@ -0,0 +1,27 @@
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div>
+      <div>
+        <img src={avatar} alt={username} />
+        <p>{username}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
