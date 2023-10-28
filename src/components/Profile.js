a8b2b9164a9925a48bd2ef3cf3e657653e71a30c
DenysTkachov <D.Tkachev@ukr.net>
2023-10-28 10:54:12 +0200

    Profile style

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,27 +1,58 @@
+import styled from 'styled-components';
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const UserInfo = styled.div`
+  text-align: center;
+`;
+
+const UserStats = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  padding: 0;
+`;
+
+const UserItem = styled.li`
+  margin: 0 20px;
+`;
+
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div>
-      <div>
+    <ProfileContainer>
+      <UserInfo>
         <img src={avatar} alt={username} />
-        <p>{username}</p>
+        <p>
+          <strong>{username}</strong>
+        </p>
         <p>{tag}</p>
         <p>{location}</p>
-      </div>
-
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
-        </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
-        </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+      </UserInfo>
+      <UserStats>
+        <UserItem>
+          <strong>Followers</strong>
+          <p>
+            <span>{stats.followers}</span>
+          </p>
+        </UserItem>
+        <UserItem>
+          <strong>Views</strong>
+          <p>
+            <span>{stats.views}</span>
+          </p>
+        </UserItem>
+        <UserItem>
+          <strong>Likes</strong>
+          <p>
+            <span>{stats.likes}</span>
+          </p>
+        </UserItem>
+      </UserStats>
+    </ProfileContainer>
   );
 };
