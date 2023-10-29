932e3dbad348141a979e84ac821ba7d75ee5d7fa
DenysTkachov <D.Tkachev@ukr.net>
2023-10-29 19:16:11 +0100

    last

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Profile } from './Profile/Profile';
-import user from '../user.json';
+import user from '../data/user.json';
 import { Statistics } from './Statistics/Statistics';
-import data from '../data.json';
+import data from '../data/data.json';
 import { FriendList } from './FriendList/FriendList';
-import friends from '../friends.json';
+import friends from '../data/friends.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import transactions from '../transactions.json';
+import transactions from '../data/transactions.json';
 
 
 export const App = () => {
